efd56828bfc05802ee84a21128890e08798760de
Lenovo <Lenovo@DESKTOP-JSE715S>
2023-06-02 13:54:46 +0530

    first commit

---
+++ CoreJava1/src/DailyPractice/SwappingUsingTwoVariables.java
@@ -0,0 +1,19 @@
+package DailyPractice;
+
+public class SwappingUsingTwoVariables {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+	
+		a=a+b;  //a=10+20=30
+		b=a-b;  //b=30-20=10
+		a=a-b;   //a=30-10=20
+		
+		System.out.println("The Value of a is : "+a);
+		System.out.println("The Value of b is : "+b);
+
+	}
+
+}
