22398c65176894f90713da816e109f7c98f759cf
Lenovo <Lenovo@DESKTOP-JSE715S>
2023-06-02 14:21:55 +0530

    just commit

--- CoreJava1/src/DailyPractice/SwappingUsingTwoVariables.java
+++ CoreJava1/src/DailyPractice/SwappingUsingTwoVariables.java
@@ -5,14 +5,21 @@ public class SwappingUsingTwoVariables {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
+		int c;
+		
+		/* c=a;  //c=10
+		 a=b;  //a=20
+		 b=c;  //b=10  */
 		
 	
-		a=a+b;  //a=10+20=30
+		/*  a=a+b;  //a=10+20=30
 		b=a-b;  //b=30-20=10
-		a=a-b;   //a=30-10=20
+		a=a-b;   //a=30-10=20  */
 		
 		System.out.println("The Value of a is : "+a);
 		System.out.println("The Value of b is : "+b);
+		
+		
 
 	}
 
